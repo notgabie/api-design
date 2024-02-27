5b179c7bdebbd940ff3349057022766621e558da
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-02-27 22:21:44 +0100

    add custom error handling

--- src/server.ts
+++ src/server.ts
@@ -17,4 +17,14 @@ app.get("/", (req, res) => {
 app.use("/api", protect, router);
 app.post("/user", createNewUser);
 app.post("/signin", signin);
+
+app.use((err, req, res, next) => {
+  if (err.type === 'auth') {
+    res.status(401).json({ message: "unauthorized" })
+  } else if (err.type === "input") {
+    res.status(400).json({message: "invalid input"})
+  } else {
+    res.status(500).json({message: 'oops, that\'s on us!'})
+  }
+})
 export default app;
